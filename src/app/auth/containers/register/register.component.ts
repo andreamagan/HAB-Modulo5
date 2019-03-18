ca18571403d030f1c11e78fe31d47cb7d82d47c0
Andrea <andrea.magan@outlook.com>
2019-03-18 21:15:56 +0100

    add login form and register form

--- src/app/auth/containers/register/register.component.ts
+++ src/app/auth/containers/register/register.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'sn-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "sn-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  registerForm = this.fb.group({
+    fullName: ["", [Validators.required]],
+    email: ["", [Validators.required, Validators.email]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ["", [Validators.required, Validators.minLength(6)]]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit() {
+    // this.loginForm.valueChanges.subscribe(data => console.log(data));
   }
 
+  register() {
+    if (this.registerForm.valid) {
+      this.authService
+        .register(this.registerForm.value)
+        .subscribe(data => console.log(data), error => console.log(error));
+    }
+  }
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login({ email, password }) {
+    return this.http.post(`${environment.apiBaseUrl}/account/login`, {
+      email,
+      password
+    });
+  }
+
+  register({ fullName, email, password }) {
+    return this.http.post(`${environment.apiBaseUrl}/account`, {
+      fullName,
+      email,
+      password
+    });
+  }
+}
